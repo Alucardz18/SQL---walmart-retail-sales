49f0fc2983af1fa3b3e96f96d385830222efb578
Nii Oye Kpakpo <166158861+Alucardz18@users.noreply.github.com>
2025-09-20 01:55:51 -0400

    Update total_sales.sql
    top 10 highest total sales

--- walmart_retail_analysis/total_sales.sql
+++ walmart_retail_analysis/total_sales.sql
@@ -1,7 +1,6 @@
 -- Which store generates the highest total sales over the period?
 SELECT Store, SUM(Weekly_Sales) AS total_sales
 FROM walmart_store_sales
-WHERE Date BETWEEN '2010-01-01' AND '2012-12-31'
 GROUP BY Store
 ORDER BY total_sales DESC
-LIMIT 1;
+LIMIT 10;
